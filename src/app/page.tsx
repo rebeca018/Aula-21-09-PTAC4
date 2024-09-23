e02a74d0772672560ed3f787afa1a5c6f75327f0
Rebeca <rebecasnascimento018@gmail.com>
2024-09-23 08:18:22 -0400

    Commit rota para home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,22 @@
 'use client'
 
-import Image from "next/image";
 import styles from "./page.module.css";
 import { useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import Link from 'next/link';
 
 export default function Home() {
-  const [user, setUser] = useState("");
-  const [senha, setSenha] = useState("");
-  const [erroLogin, setErroLogin] = useState(false);
+  
+  const router = useRouter();
 
-  const autenticar = () => {
-    if (user !== "teste" || senha !== "teste123"){
-      setErroLogin(true)
-    }else{
-      setErroLogin(false)
-    }
+  const click = () => {
+    router.push('/login');
   }
 
   return(
     <div>
-        <h1>Login</h1>
-        <input className={styles.input} type="text" value={user} placeholder="Nome de usuário" onChange={(e) => setUser(e.target.value)}/>
-        <input className={styles.input} type="text" value={senha} placeholder="Senha" onChange={(e) => setSenha(e.target.value)}/>
-        <button className={styles.botao} onClick={autenticar}>Login</button>
-
-        {erroLogin && (
-        <div className={styles.autenticar}>
-          <h2>Não foi possível realizar o Login</h2>
-          <p>Usuário ou Senha inválidos</p>
-        </div>
-      )}
+        <h1>Home</h1>
+        <button onClick={click} className={styles.botao}>Login</button>
     </div>
   );
 
